73db28ef8a14f40486dac1bd39989a284e5d96e9
Andrew Nguyen <anguy080@ucr.edu>
2015-08-10 15:03:58 -0700

    set up parse

--- Fakestagram/AppDelegate.m
+++ Fakestagram/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import <Parse/Parse.h>
 
 @interface AppDelegate ()
 
@@ -16,7 +17,17 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    // [Optional] Power your app with Local Datastore. For more info, go to
+    // https://parse.com/docs/ios_guide#localdatastore/iOS
+    [Parse enableLocalDatastore];
+
+    // Initialize Parse.
+    [Parse setApplicationId:@"5jUp45f4hulcsAHYDU9cnlNsUlEWSpHKO5SPiJqm"
+                  clientKey:@"9KGzv2EEYE5N7ok7czMu1NpTOncOuNEH9mDnZScO"];
+
+    // [Optional] Track statistics around application opens.
+    [PFAnalytics trackAppOpenedWithLaunchOptions:launchOptions];
+
     return YES;
 }
 
